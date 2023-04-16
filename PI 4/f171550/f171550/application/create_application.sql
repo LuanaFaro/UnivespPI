75054f34fc43874316b6b1e96187ac05ed99af14
Luana Faro <luanabif9@gmail.com>
2023-04-16 19:25:03 -0300

    Criando gráficos

--- PI 4/f171550/f171550/application/create_application.sql
+++ PI 4/f171550/f171550/application/create_application.sql
@@ -47,7 +47,7 @@ wwv_flow_imp.create_flow(
 ,p_substitution_string_01=>'APP_NAME'
 ,p_substitution_value_01=>'Projeto + Vida'
 ,p_last_updated_by=>'2008214@ALUNO.UNIVESP.BR'
-,p_last_upd_yyyymmddhh24miss=>'20230408195059'
+,p_last_upd_yyyymmddhh24miss=>'20230416221911'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>7
 ,p_print_server_type=>'INSTANCE'
--- PI 4/f171550/f171550/application/pages/page_00001.sql
+++ PI 4/f171550/f171550/application/pages/page_00001.sql
@@ -44,7 +44,7 @@ wwv_flow_imp_page.create_page(
 ,p_protection_level=>'C'
 ,p_page_component_map=>'13'
 ,p_last_updated_by=>'2008214@ALUNO.UNIVESP.BR'
-,p_last_upd_yyyymmddhh24miss=>'20230408195059'
+,p_last_upd_yyyymmddhh24miss=>'20230416221551'
 );
 wwv_flow_imp_page.create_page_plug(
  p_id=>wwv_flow_imp.id(14492528071589460721)
@@ -86,8 +86,9 @@ wwv_flow_imp_page.create_page_plug(
 '  FROM PI4_IND_MORTALIDADE_INFANTIL ',
 ' WHERE PAIS      = :P1_PAIS ',
 '   AND ANO       = :P1_ANO',
-'   AND INDICADOR = ''Deaths 1-59 months''',
-'   AND GENERO    = ''Total'''))
+'   AND INDICADOR = ''Mortes 1-59 meses''',
+'   AND GENERO    = ''Total''',
+'   AND ROWNUM    = 1'))
 ,p_lazy_loading=>false
 ,p_plug_source_type=>'NATIVE_CARDS'
 ,p_ajax_items_to_submit=>'P1_ANO,P1_PAIS'
@@ -125,8 +126,9 @@ wwv_flow_imp_page.create_page_plug(
 '  FROM PI4_IND_MORTALIDADE_INFANTIL ',
 ' WHERE PAIS      = :P1_PAIS',
 '   AND ANO       = :P1_ANO',
-'   AND INDICADOR = ''Deaths aged 10 to 14''',
-'   AND GENERO    = ''Total'''))
+'   AND INDICADOR = ''Mortes de 10 a 14 anos''',
+'   AND GENERO    = ''Total''',
+'   AND ROWNUM    = 1'))
 ,p_lazy_loading=>false
 ,p_plug_source_type=>'NATIVE_CARDS'
 ,p_ajax_items_to_submit=>'P1_ANO,P1_PAIS'
@@ -164,8 +166,9 @@ wwv_flow_imp_page.create_page_plug(
 '  FROM PI4_IND_MORTALIDADE_INFANTIL ',
 ' WHERE PAIS      = :P1_PAIS',
 '   AND ANO       = :P1_ANO',
-'   AND INDICADOR = ''Child deaths (aged 1-4 years)''',
-'   AND GENERO    = ''Total'''))
+unistr('   AND INDICADOR = ''\00D3bitos infantis (de 1 a 4 anos)'''),
+'   AND GENERO    = ''Total''',
+'   AND ROWNUM    = 1'))
 ,p_lazy_loading=>false
 ,p_plug_source_type=>'NATIVE_CARDS'
 ,p_ajax_items_to_submit=>'P1_ANO,P1_PAIS'
@@ -203,8 +206,9 @@ wwv_flow_imp_page.create_page_plug(
 '  FROM PI4_IND_MORTALIDADE_INFANTIL ',
 ' WHERE PAIS      = :P1_PAIS',
 '   AND ANO       = :P1_ANO',
-'   AND INDICADOR = ''Deaths aged 5 to 9''',
-'   AND GENERO    = ''Total'''))
+'   AND INDICADOR = ''Mortes de 5 a 9 anos''',
+'   AND GENERO    = ''Total''',
+'   AND ROWNUM    = 1'))
 ,p_lazy_loading=>false
 ,p_plug_source_type=>'NATIVE_CARDS'
 ,p_ajax_items_to_submit=>'P1_ANO,P1_PAIS'
@@ -242,8 +246,10 @@ wwv_flow_imp_page.create_page_plug(
 '  FROM PI4_IND_MORTALIDADE_INFANTIL ',
 ' WHERE PAIS      = :P1_PAIS',
 '   AND ANO       = :P1_ANO',
-'   AND INDICADOR = ''Deaths aged 15 to 19''',
-'   AND GENERO    = ''Total'''))
+'   AND INDICADOR = ''Mortes entre 15 e 19 anos''',
+'   AND GENERO    = ''Total''',
+'   AND ROWNUM    = 1',
+''))
 ,p_lazy_loading=>false
 ,p_plug_source_type=>'NATIVE_CARDS'
 ,p_ajax_items_to_submit=>'P1_ANO,P1_PAIS'
@@ -265,6 +271,356 @@ wwv_flow_imp_page.create_card(
 ,p_body_adv_formatting=>false
 ,p_second_body_adv_formatting=>false
 ,p_media_adv_formatting=>false
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(40682321745556277746)
+,p_plug_name=>unistr('Total de mortes por g\00EAnero')
+,p_region_template_options=>'#DEFAULT#:t-Region--scrollBody'
+,p_component_template_options=>'#DEFAULT#'
+,p_escape_on_http_output=>'Y'
+,p_plug_template=>wwv_flow_imp.id(69137296463532422097)
+,p_plug_display_sequence=>20
+,p_include_in_reg_disp_sel_yn=>'Y'
+,p_plug_source_type=>'NATIVE_JET_CHART'
+,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
+);
+wwv_flow_imp_page.create_jet_chart(
+ p_id=>wwv_flow_imp.id(40682321893425277747)
+,p_region_id=>wwv_flow_imp.id(40682321745556277746)
+,p_chart_type=>'bar'
+,p_height=>'400'
+,p_animation_on_display=>'auto'
+,p_animation_on_data_change=>'auto'
+,p_orientation=>'vertical'
+,p_data_cursor=>'auto'
+,p_data_cursor_behavior=>'auto'
+,p_hover_behavior=>'dim'
+,p_stack=>'off'
+,p_stack_label=>'off'
+,p_connect_nulls=>'Y'
+,p_value_position=>'auto'
+,p_sorting=>'label-asc'
+,p_fill_multi_series_gaps=>true
+,p_zoom_and_scroll=>'off'
+,p_tooltip_rendered=>'Y'
+,p_show_series_name=>true
+,p_show_group_name=>true
+,p_show_value=>true
+,p_show_label=>true
+,p_show_row=>true
+,p_show_start=>true
+,p_show_end=>true
+,p_show_progress=>true
+,p_show_baseline=>true
+,p_legend_rendered=>'off'
+,p_legend_position=>'auto'
+,p_overview_rendered=>'off'
+,p_horizontal_grid=>'auto'
+,p_vertical_grid=>'auto'
+,p_gauge_orientation=>'circular'
+,p_gauge_plot_area=>'on'
+,p_show_gauge_value=>true
+);
+wwv_flow_imp_page.create_jet_chart_series(
+ p_id=>wwv_flow_imp.id(40682321930203277748)
+,p_chart_id=>wwv_flow_imp.id(40682321893425277747)
+,p_seq=>10
+,p_name=>'Feminino'
+,p_data_source_type=>'SQL'
+,p_data_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select sum(valor) as total, Indicador, GENERO from PI4_IND_MORTALIDADE_INFANTIL',
+'where pais = :P1_PAIS',
+'and genero = ''Feminino''',
+'and ano = :P1_ANO',
+unistr('and INDICADOR IN (''\00D3bitos infantis (de 1 a 4 anos)'''),
+'                   ,''Mortes de 5 a 9 anos''',
+'                   ,''Mortes de 10 a 14 anos''',
+'                   ,''Mortes entre 15 e 19 anos'')',
+'                   Group By INDICADOR, GENERO',
+'',
+'',
+'',
+''))
+,p_ajax_items_to_submit=>'P1_ANO,P1_PAIS'
+,p_items_value_column_name=>'TOTAL'
+,p_items_label_column_name=>'INDICADOR'
+,p_assigned_to_y2=>'off'
+,p_items_label_rendered=>false
+,p_items_label_display_as=>'PERCENT'
+,p_threshold_display=>'onIndicator'
+);
+wwv_flow_imp_page.create_jet_chart_series(
+ p_id=>wwv_flow_imp.id(73711465422362238801)
+,p_chart_id=>wwv_flow_imp.id(40682321893425277747)
+,p_seq=>20
+,p_name=>'Masculino'
+,p_data_source_type=>'SQL'
+,p_data_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select sum(valor) as total, Indicador, GENERO from PI4_IND_MORTALIDADE_INFANTIL',
+'where pais = :P1_PAIS',
+'and genero = ''Masculino''',
+'and ano = :P1_ANO',
+unistr('and INDICADOR IN (''\00D3bitos infantis (de 1 a 4 anos)'''),
+'                   ,''Mortes de 5 a 9 anos''',
+'                   ,''Mortes de 10 a 14 anos''',
+'                   ,''Mortes entre 15 e 19 anos'')',
+'                   Group By INDICADOR, GENERO',
+'',
+'',
+'',
+''))
+,p_ajax_items_to_submit=>'P1_ANO,P1_PAIS'
+,p_items_value_column_name=>'TOTAL'
+,p_items_label_column_name=>'INDICADOR'
+,p_assigned_to_y2=>'off'
+,p_items_label_rendered=>false
+,p_items_label_display_as=>'PERCENT'
+,p_threshold_display=>'onIndicator'
+);
+wwv_flow_imp_page.create_jet_chart_axis(
+ p_id=>wwv_flow_imp.id(40682322017623277749)
+,p_chart_id=>wwv_flow_imp.id(40682321893425277747)
+,p_axis=>'x'
+,p_is_rendered=>'on'
+,p_format_scaling=>'auto'
+,p_scaling=>'linear'
+,p_baseline_scaling=>'zero'
+,p_major_tick_rendered=>'on'
+,p_minor_tick_rendered=>'off'
+,p_tick_label_rendered=>'on'
+,p_tick_label_rotation=>'auto'
+,p_tick_label_position=>'outside'
+,p_zoom_order_seconds=>false
+,p_zoom_order_minutes=>false
+,p_zoom_order_hours=>false
+,p_zoom_order_days=>false
+,p_zoom_order_weeks=>false
+,p_zoom_order_months=>false
+,p_zoom_order_quarters=>false
+,p_zoom_order_years=>false
+);
+wwv_flow_imp_page.create_jet_chart_axis(
+ p_id=>wwv_flow_imp.id(40682322108311277750)
+,p_chart_id=>wwv_flow_imp.id(40682321893425277747)
+,p_axis=>'y'
+,p_is_rendered=>'on'
+,p_format_type=>'decimal'
+,p_decimal_places=>0
+,p_format_scaling=>'none'
+,p_scaling=>'linear'
+,p_baseline_scaling=>'zero'
+,p_position=>'auto'
+,p_major_tick_rendered=>'on'
+,p_minor_tick_rendered=>'off'
+,p_tick_label_rendered=>'on'
+,p_zoom_order_seconds=>false
+,p_zoom_order_minutes=>false
+,p_zoom_order_hours=>false
+,p_zoom_order_days=>false
+,p_zoom_order_weeks=>false
+,p_zoom_order_months=>false
+,p_zoom_order_quarters=>false
+,p_zoom_order_years=>false
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(73711466221321238809)
+,p_plug_name=>unistr('Top 5 Pa\00EDses com maior n\00FAmero de mortes')
+,p_region_template_options=>'#DEFAULT#:t-Region--scrollBody'
+,p_component_template_options=>'#DEFAULT#'
+,p_escape_on_http_output=>'Y'
+,p_plug_template=>wwv_flow_imp.id(69137296463532422097)
+,p_plug_display_sequence=>30
+,p_include_in_reg_disp_sel_yn=>'Y'
+,p_plug_new_grid_row=>false
+,p_plug_source_type=>'NATIVE_JET_CHART'
+,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
+);
+wwv_flow_imp_page.create_jet_chart(
+ p_id=>wwv_flow_imp.id(73711466337967238810)
+,p_region_id=>wwv_flow_imp.id(73711466221321238809)
+,p_chart_type=>'bar'
+,p_height=>'400'
+,p_animation_on_display=>'auto'
+,p_animation_on_data_change=>'auto'
+,p_orientation=>'vertical'
+,p_data_cursor=>'auto'
+,p_data_cursor_behavior=>'auto'
+,p_hover_behavior=>'dim'
+,p_stack=>'off'
+,p_stack_label=>'off'
+,p_connect_nulls=>'Y'
+,p_value_position=>'auto'
+,p_sorting=>'label-asc'
+,p_fill_multi_series_gaps=>true
+,p_zoom_and_scroll=>'off'
+,p_tooltip_rendered=>'Y'
+,p_show_series_name=>true
+,p_show_group_name=>true
+,p_show_value=>true
+,p_show_label=>true
+,p_show_row=>true
+,p_show_start=>true
+,p_show_end=>true
+,p_show_progress=>true
+,p_show_baseline=>true
+,p_legend_rendered=>'off'
+,p_legend_position=>'auto'
+,p_overview_rendered=>'off'
+,p_horizontal_grid=>'auto'
+,p_vertical_grid=>'auto'
+,p_gauge_orientation=>'circular'
+,p_gauge_plot_area=>'on'
+,p_show_gauge_value=>true
+);
+wwv_flow_imp_page.create_jet_chart_series(
+ p_id=>wwv_flow_imp.id(73711466454862238811)
+,p_chart_id=>wwv_flow_imp.id(73711466337967238810)
+,p_seq=>10
+,p_name=>'Geral'
+,p_data_source_type=>'SQL'
+,p_data_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select total, pais from (',
+'    select * from (',
+'        select sum(Valor)as Total, Pais From PI4_IND_MORTALIDADE_INFANTIL',
+'          Where ano = :P1_ANO',
+'          and',
+'          INDICADOR IN (''Mortes 1-59 meses''',
+unistr('                       ,''\00D3bitos infantis (de 1 a 4 anos)'''),
+'                       ,''Mortes de 5 a 9 anos''',
+'                       ,''Mortes de 10 a 14 anos''',
+'                       ,''Mortes entre 15 e 19 anos'')',
+'          Group by Pais',
+')',
+'Order By Total desc',
+')',
+'where rownum <=5',
+'',
+''))
+,p_ajax_items_to_submit=>'P1_ANO'
+,p_items_value_column_name=>'TOTAL'
+,p_items_label_column_name=>'PAIS'
+,p_assigned_to_y2=>'off'
+,p_items_label_rendered=>false
+,p_items_label_display_as=>'PERCENT'
+,p_threshold_display=>'onIndicator'
+);
+wwv_flow_imp_page.create_jet_chart_axis(
+ p_id=>wwv_flow_imp.id(73711466676731238813)
+,p_chart_id=>wwv_flow_imp.id(73711466337967238810)
+,p_axis=>'x'
+,p_is_rendered=>'on'
+,p_format_scaling=>'auto'
+,p_scaling=>'linear'
+,p_baseline_scaling=>'zero'
+,p_major_tick_rendered=>'on'
+,p_minor_tick_rendered=>'off'
+,p_tick_label_rendered=>'on'
+,p_tick_label_rotation=>'auto'
+,p_tick_label_position=>'outside'
+,p_zoom_order_seconds=>false
+,p_zoom_order_minutes=>false
+,p_zoom_order_hours=>false
+,p_zoom_order_days=>false
+,p_zoom_order_weeks=>false
+,p_zoom_order_months=>false
+,p_zoom_order_quarters=>false
+,p_zoom_order_years=>false
+);
+wwv_flow_imp_page.create_jet_chart_axis(
+ p_id=>wwv_flow_imp.id(73711466786797238814)
+,p_chart_id=>wwv_flow_imp.id(73711466337967238810)
+,p_axis=>'y'
+,p_is_rendered=>'on'
+,p_format_type=>'decimal'
+,p_decimal_places=>0
+,p_format_scaling=>'none'
+,p_scaling=>'linear'
+,p_baseline_scaling=>'zero'
+,p_position=>'auto'
+,p_major_tick_rendered=>'on'
+,p_minor_tick_rendered=>'off'
+,p_tick_label_rendered=>'on'
+,p_zoom_order_seconds=>false
+,p_zoom_order_minutes=>false
+,p_zoom_order_hours=>false
+,p_zoom_order_days=>false
+,p_zoom_order_weeks=>false
+,p_zoom_order_months=>false
+,p_zoom_order_quarters=>false
+,p_zoom_order_years=>false
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(73711466954569238816)
+,p_plug_name=>unistr('Total de mortes por ano/pa\00EDs')
+,p_region_template_options=>'#DEFAULT#:t-Region--scrollBody'
+,p_component_template_options=>'#DEFAULT#'
+,p_escape_on_http_output=>'Y'
+,p_plug_template=>wwv_flow_imp.id(69137296463532422097)
+,p_plug_display_sequence=>40
+,p_include_in_reg_disp_sel_yn=>'Y'
+,p_plug_source_type=>'NATIVE_JET_CHART'
+,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
+);
+wwv_flow_imp_page.create_jet_chart(
+ p_id=>wwv_flow_imp.id(73711467015569238817)
+,p_region_id=>wwv_flow_imp.id(73711466954569238816)
+,p_chart_type=>'pie'
+,p_height=>'400'
+,p_animation_on_display=>'auto'
+,p_animation_on_data_change=>'auto'
+,p_data_cursor=>'auto'
+,p_data_cursor_behavior=>'auto'
+,p_hide_and_show_behavior=>'withRescale'
+,p_hover_behavior=>'dim'
+,p_stack=>'off'
+,p_stack_label=>'off'
+,p_connect_nulls=>'Y'
+,p_value_position=>'auto'
+,p_value_format_type=>'decimal'
+,p_value_decimal_places=>0
+,p_value_format_scaling=>'none'
+,p_sorting=>'label-asc'
+,p_fill_multi_series_gaps=>true
+,p_tooltip_rendered=>'Y'
+,p_show_series_name=>true
+,p_show_group_name=>true
+,p_show_value=>true
+,p_show_label=>true
+,p_show_row=>true
+,p_show_start=>true
+,p_show_end=>true
+,p_show_progress=>true
+,p_show_baseline=>true
+,p_legend_rendered=>'on'
+,p_legend_position=>'auto'
+,p_overview_rendered=>'off'
+,p_pie_other_threshold=>0
+,p_pie_selection_effect=>'highlight'
+,p_horizontal_grid=>'auto'
+,p_vertical_grid=>'auto'
+,p_gauge_orientation=>'circular'
+,p_gauge_plot_area=>'on'
+,p_show_gauge_value=>true
+);
+wwv_flow_imp_page.create_jet_chart_series(
+ p_id=>wwv_flow_imp.id(73711467128869238818)
+,p_chart_id=>wwv_flow_imp.id(73711467015569238817)
+,p_seq=>10
+,p_name=>'Geral'
+,p_data_source_type=>'SQL'
+,p_data_source=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'SELECT SUM(VALOR) AS TOTAL,PAIS  FROM PI4_IND_MORTALIDADE_INFANTIL ',
+'WHERE GENERO = ''Total''',
+'AND ANO = :P1_ANO',
+'GROUP BY PAIS',
+'ORDER BY PAIS'))
+,p_ajax_items_to_submit=>'P1_ANO'
+,p_items_value_column_name=>'TOTAL'
+,p_items_label_column_name=>'PAIS'
+,p_items_label_rendered=>true
+,p_items_label_position=>'auto'
+,p_items_label_display_as=>'LABEL'
+,p_threshold_display=>'onIndicator'
 );
 wwv_flow_imp_page.create_page_item(
  p_id=>wwv_flow_imp.id(14492529450986460735)
@@ -301,12 +657,7 @@ wwv_flow_imp_page.create_page_item(
 ,p_name=>'P1_PAIS'
 ,p_item_sequence=>20
 ,p_item_plug_id=>wwv_flow_imp.id(14492529570565460736)
-,p_item_default=>wwv_flow_string.join(wwv_flow_t_varchar2(
-'SELECT PAIS',
-'  FROM PI4_IND_MORTALIDADE_INFANTIL ',
-' WHERE PAIS = ''Brazil''',
-''))
-,p_item_default_type=>'SQL_QUERY'
+,p_item_default=>'Brasil'
 ,p_prompt=>unistr('Pa\00EDs')
 ,p_display_as=>'NATIVE_POPUP_LOV'
 ,p_lov=>wwv_flow_string.join(wwv_flow_t_varchar2(
@@ -387,6 +738,36 @@ wwv_flow_imp_page.create_page_da_action(
 ,p_action=>'NATIVE_REFRESH'
 ,p_affected_elements_type=>'REGION'
 ,p_affected_region_id=>wwv_flow_imp.id(14492529663379460737)
+);
+wwv_flow_imp_page.create_page_da_action(
+ p_id=>wwv_flow_imp.id(73711465527690238802)
+,p_event_id=>wwv_flow_imp.id(14492529966457460740)
+,p_event_result=>'TRUE'
+,p_action_sequence=>60
+,p_execute_on_page_init=>'N'
+,p_action=>'NATIVE_REFRESH'
+,p_affected_elements_type=>'REGION'
+,p_affected_region_id=>wwv_flow_imp.id(40682321745556277746)
+);
+wwv_flow_imp_page.create_page_da_action(
+ p_id=>wwv_flow_imp.id(73711466896235238815)
+,p_event_id=>wwv_flow_imp.id(14492529966457460740)
+,p_event_result=>'TRUE'
+,p_action_sequence=>70
+,p_execute_on_page_init=>'N'
+,p_action=>'NATIVE_REFRESH'
+,p_affected_elements_type=>'REGION'
+,p_affected_region_id=>wwv_flow_imp.id(73711466221321238809)
+);
+wwv_flow_imp_page.create_page_da_action(
+ p_id=>wwv_flow_imp.id(73711467419547238821)
+,p_event_id=>wwv_flow_imp.id(14492529966457460740)
+,p_event_result=>'TRUE'
+,p_action_sequence=>80
+,p_execute_on_page_init=>'N'
+,p_action=>'NATIVE_REFRESH'
+,p_affected_elements_type=>'REGION'
+,p_affected_region_id=>wwv_flow_imp.id(73711466954569238816)
 );
 wwv_flow_imp.component_end;
 end;
